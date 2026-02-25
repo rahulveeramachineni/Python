6aa3a8aaa14dcaf5a2066e4dabf59b28d45a9faa
Rahul <rahulvneni@gmail.com>
2026-02-25 15:09:20 +0530

    Add files via upload
    Assignment_1

---
+++ Task_1.py
@@ -0,0 +1,22 @@
+"""
+        Addition
+        subtraction
+        Multiplication
+        Division
+
+"""
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+Addition = x+y
+Subtraction= x-y
+Multiplication = x * y
+Division = x/y
+
+print("Addition: ",Addition)
+print("subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",Division)
+
+
+
